dbbc97df12d58496d970d2a01f7f8b3b28be9bf3
spam1312 <46295956+spam1312@users.noreply.github.com>
2019-09-16 23:31:33 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const client2 = new Discord.Client();
-const client3 = new Discord.Client();
-const client4 = new Discord.Client();
-const client5 = new Discord.Client();
 const myid = ['487340960058179604'];
 const prefix = ['!'];
 
@@ -132,6 +129,3 @@ if (!rank) return message.reply(' ')
 
 client.login(process.env.TOKEN);
 client2.login(process.env.TOKEN2);
-client3.login(process.env.TOKEN3);
-client4.login(process.env.TOKEN4);
-client5.login(process.env.TOKEN5);
